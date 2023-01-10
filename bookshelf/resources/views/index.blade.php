bef58675fa07e44ddbf45a8d284ded1c12b42e10
junior41 <juniorbrandao41@hotmail.com>
2023-01-10 17:18:33 -0300

    sistema de autenticacao

---
+++ bookshelf/resources/views/index.blade.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PhotoFolio Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- jQuery -->
+  <script src="assets/js/jquery-2.1.0.min.js"></script>
+  
+  <!-- Bootstrap -->
+  <script src="assets/js/bootstrap.min.js"></script>
+    
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="assets/css/icons.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: PhotoFolio - v1.2.0
+  * Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+
+ 
+      <span style="font-size:30px;cursor:pointer" onclick="openNav()"><img src="/assets/img/Vector.png" alt=""></span> 
+
+      <div class = "user">
+        <img src="/assets/img/perfil-de-usuario.png" alt="usuário">
+        <h5>Junior</h5>
+      </div>
+
+      <div class = "logo">
+        <img src="/assets/img/logo-removebg-preview 1.png" alt="Logo bookshelf">      
+      </div>
+    </div>
+        
+  </header><!-- End Header -->
+    <div class = "sobre">
+      <div class = "text">
+        <h1>Livraria CEUNES</h1>
+        <p>
+          É notório a falta de organização e estagnação do acervo de
+          bibliotecas nas escolas brasileiras inclusive a gestão da biblioteca de maneira
+          manual, impede que o estudante manuseie as obras e faça suas próprias
+          descobertas no local. Tendo em vista que os alunos do século XXI valorizam os
+          recursos que otimizam o tempo e tornam mais práticas as atividades diárias.
+          O software tem como propósito possibilitar aos clientes de biblioteca uma maior
+          rapidez, organização e controle de cadastros de todo acervo bibliográfico,
+          permitindo a gestão eficiente dos empréstimos de livros.
+        </p>
+        <form action="#">
+          <input type="text" placeholder = "pesquisar">
+        </form>
+      </div>
+      <div class = "img">
+        <img src="/assets/img/image 4.png" alt="">
+      </div>
+    </div>
+    <!-- ======= Gallery Section ======= -->
+    <section id="gallery" class="gallery">
+      <div class="container-fluid">
+
+        <div class="row gy-4 justify-content-center">
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <a href = "#" class="gallery-item h-100">
+              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <div class = "titulo">
+                <h5>titulo do livro</h5>
+              </div>
+              <div class = "classificacao">
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela (1).png"></img>
+                <img src = "/assets/img/estrela.png"></img>
+              </div>
+            </a>
+          </div><!-- End Gallery Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Gallery Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class = "img-logo">
+      <img src="/assets/img/image 5.png" alt="logo ufes">
+    </div>
+    <div class="container" style = "align-items:center;">
+      <div class="copyright">
+        &copy; Copyright <strong><span>UMPS, república</span></strong>. Todos os direitos reservados.
+      </div>
+      <div class="credits">
+        Designed by <a href="#">wilsin</a>
+      </div>
+    </div>
+    <div class = "contato">
+          <!-- Facebook -->
+          <a href="#"><img src="/assets/img/simbolo-de-aplicativo-do-facebook.png" alt="logo Facebook"></a>
+
+          <!-- Twitter -->
+          <a href="#"><img src="/assets/img/twitter.png" alt="logo Twitter"></a>
+
+          <!-- github -->
+          <a href="#"><img src="/assets/img/github.png" alt="logo github"></a>
+
+          <!-- Instagram -->
+          <a href="#"><img src="/assets/img/instagram.png" alt="logo instagram"></a>
+    </div>
+  </footer><!-- End Footer -->
+
+
+  <div id="preloader">
+    <div class="line"></div>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script>
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+  </script>
+
+</body>
+
+</html>
